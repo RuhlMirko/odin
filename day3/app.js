6b36d76b1aa8eeed4bf465681af1a883cc756b39
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-03-20 11:05:11 -0300

    Day 3 complete

--- day3/app.js
+++ day3/app.js
@@ -1,20 +1,32 @@
 'use strict'
-
-function plusSev(num) {
-  return num + 7;
-}
-function multiply(num1, num2) {
-  return num1 * num2;
-}
-function capitalize(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-}
-function lastLetter(str) {
-  return str.charAt(str.length - 1);
-}
+const player1 = document.querySelector('button')
+const title = document.querySelector('h1')
+const items = ['rock', 'paper', 'scissors'];
+const pcChoice = document.querySelector('span');
+let userInput = ''
 
 
-console.log(plusSev(3)); // 10
-console.log(multiply(10,2)); // 20 
-console.log(capitalize('null')); // 19
-console.log(lastLetter('hello')); // o
+player1.addEventListener('click', () => {
+  userInput = prompt('Enter your choice: rock, paper, or scissors').trim().toLowerCase();
+  title.textContent = checkWinner();
+})
+
+function checkWinner() {
+  const actualChoice = items[Math.floor(Math.random() * items.length)]
+  pcChoice.textContent = actualChoice;
+  if (userInput === actualChoice) {
+    return 'It\'s a tie!';
+  } else if (userInput === 'rock' && actualChoice === 'scissors') {
+    return 'You win!';
+  } else if (userInput === 'paper' && actualChoice === 'rock') {
+    return 'You win!';
+  } else if (userInput === 'scissors' && actualChoice === 'paper') {
+    return 'You win!';
+  } else {
+    return 'You lose!';
+  }
+}
+
+
+
+console.log(userInput)
