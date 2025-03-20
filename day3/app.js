5e293672338df05c447894cbc4626cba1de2d77d
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-03-20 11:32:59 -0300

    Added a 5 round limit

--- day3/app.js
+++ day3/app.js
@@ -5,33 +5,57 @@ const items = ['rock', 'paper', 'scissors'];
 const pcChoice = document.querySelector('span');
 let userInput = ''
 let score = 0;
+let rounds = 0
 const scoreElement = document.getElementById('score');
+const roundElement = document.getElementById('rounds');
+
 
 player1.addEventListener('click', () => {
   userInput = prompt('Enter your choice: rock, paper, or scissors').trim().toLowerCase();
-  title.textContent = checkWinner();
+  title.textContent = checkRound();
 })
 
-function checkWinner() {
+function checkRound() {
   const actualChoice = items[Math.floor(Math.random() * items.length)]
   pcChoice.textContent = actualChoice;
+  rounds++;
+  roundElement.textContent = rounds;
+
   if (userInput === actualChoice) {
     return 'It\'s a tie!';
   } else if (userInput === 'rock' && actualChoice === 'scissors') {
-    score++;
-    scoreElement.textContent = score;
+    updateScore()
     return 'You win!';
   } else if (userInput === 'paper' && actualChoice === 'rock') {
-    score++;
-    scoreElement.textContent = score;
+    updateScore()
     return 'You win!';
   } else if (userInput === 'scissors' && actualChoice === 'paper') {
-    score++;
-    scoreElement.textContent = score;
+    updateScore();
     return 'You win!';
   } else {
     score--;
     scoreElement.textContent = score;
+    checkWinner();
     return 'You lose!';
   }
+}
+
+function checkWinner() {
+  if (score >= 3) {
+    title.textContent = 'You win the game!';
+    player1.disabled = true;
+    console.log('case 1')
+  } else if (rounds === 5) {
+    title.textContent = 'You lost the game'
+    player1.disabled = true;
+    console.log('case 2')
+  }
+
+}
+
+
+function updateScore() {
+  score++;
+  scoreElement.textContent = score;
+  checkWinner();
 }
