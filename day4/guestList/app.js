80a5c8bb956dad3e2093750664fc4be90fd614f1
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-03-21 15:57:21 -0300

    Added for loop to filter the refused people

--- day4/guestList/app.js
+++ day4/guestList/app.js
@@ -1,1 +1,15 @@
 'use strict'
+const admitList = document.getElementById('admitList');
+const refuseList = document.getElementById('refuseList');
+const peopleList = ['Chris', 'Anne', 'Colin', 'Terri', 'Phil', 'Lola', 'Sam', 'Kay', 'Bruce']
+
+
+for (let person of peopleList) {
+    if (person === 'Phil' || person === 'Lola') {
+        refuseList.innerHTML += `<li>${person}</li>`;
+    }
+    else {
+        admitList.innerHTML += `<li>${person}</li>`;
+    }
+}
+
