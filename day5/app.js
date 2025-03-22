5c10f875442515e09402634b1f600e43138ca840
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-03-22 12:49:40 -0300

    Working: game logic working, it only needs to stop receiving inputs after the 5 rounds

--- day5/app.js
+++ day5/app.js
@@ -2,6 +2,10 @@
 const choices = ['rock', 'paper', 'scissors']
 const cpuDisplay = document.querySelector('#cpu')
 const playerMenu = document.querySelector('.player')
+let score = 0
+let round = 0
+const scoreDisplay = document.querySelector('#score')
+const roundDisplay = document.querySelector('#rounds')
 /*
 
 let menu = document.querySelector('#menu');
@@ -45,13 +49,34 @@ playerMenu.addEventListener('click', (e) => {
 const checkRound = function (userInput) {
     const cpuInput = choices[Math.floor((Math.random() * choices.length))]
     cpuDisplay.textContent = cpuInput
+    roundDisplay.textContent = ++round
 
-    switch (userInput) {
-        case 'rock':
-            if (cpuInput == 'rock') {
-                return 'Draw'
-            }
+    if (userInput == cpuInput) {
+        cpuDisplay.textContent += ' (Draw)'
+    } else if (userInput === 'rock' && cpuInput === 'scissors') {
+        cpuDisplay.textContent += ' (You win!)'
+        score++
+    } else if (userInput === 'paper' && cpuInput === 'rock') {
+        cpuDisplay.textContent += ' (You win!)'
+        score++
+    } else if (userInput === 'scissors' && cpuInput === 'paper') {
+        cpuDisplay.textContent += ' (You win!)'
+        score++
+    } else {
+        cpuDisplay.textContent += ' (You lost!)'
     }
 
+    scoreDisplay.textContent = score
+    if (round === 5) {
+        checkWinner()
+    }
     console.log(cpuInput, userInput);
+}
+
+const checkWinner = function () {
+    if (score >= 3) {
+        cpuDisplay.textContent = 'Player wins!'
+    } else {
+        cpuDisplay.textContent = 'CPU wins!'
+    }
 }
