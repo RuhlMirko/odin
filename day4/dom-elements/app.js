c694e6c20a62fdcc92ac4e71a404fffd9e1c82c4
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-03-22 00:35:09 -0300

    Practice: Creating and appending elements to html

--- day4/dom-elements/app.js
+++ day4/dom-elements/app.js
@@ -1,12 +1,33 @@
-'use strict'
-
-// selects the #container div (don't worry about the syntax, we'll get there)
+// your JavaScript file
 const container = document.querySelector("#container");
-// selects the .controls div
-const controls = document.querySelector(".controls");
-// selects the first child of #container => .display
-const display = container.firstElementChild;
-console.log(display);  // <div class="display"></div>
+
+const content = document.createElement("div");
+content.classList.add("content");
+content.textContent = "This is the glorious text-content!";
+
+container.appendChild(content);
 
 
+let newP = document.createElement('p')
+newP.textContent = "Hey I'm red!"
+newP.style.color = 'red'
+container.appendChild(newP)
+
+let newh3 = document.createElement('h3')
+newh3.textContent = "I'm a blue h3!"
+newh3.style.color = 'blue'
+container.appendChild(newh3)
+
+let newDiv = document.createElement('div')
+newDiv.style.border = 'solid 1px black'
+newDiv.style.backgroundColor = 'pink'
+let insideH1 = document.createElement('h1')
+insideH1.textContent = "I'm inside a div!"
+let insideP = document.createElement('p')
+insideP.textContent = 'Me too!'
+
+
+newDiv.appendChild(insideH1)
+newDiv.appendChild(insideP)
+container.appendChild(newDiv)
 
