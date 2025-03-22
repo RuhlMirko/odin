4dcd157b98713382a192fa478ebd691ee850acfa
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-03-22 00:43:16 -0300

    Practice: tried and tested add event listeners

--- day4/dom-elements/app.js
+++ day4/dom-elements/app.js
@@ -31,3 +31,19 @@ newDiv.appendChild(insideH1)
 newDiv.appendChild(insideP)
 container.appendChild(newDiv)
 
+const btn = document.querySelectorAll("button");
+
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll("button");
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+    // and for each one we add a 'click' listener
+    button.addEventListener("click", () => {
+        alert(button.id);
+    });
+});
+
+
+
+
