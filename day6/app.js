dc6a4e5714140e5ebbaf541e4e88f64c66645cc9
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-03-23 13:35:24 -0300

    Working: js successfully makes the 16x16 grid

--- day6/app.js
+++ day6/app.js
@@ -1,3 +1,19 @@
 'use strict'
 
 const container = document.querySelector('#table')
+const height = 16
+const width = 16
+
+
+for (let j = 0; j < height; j++) {
+    const row = document.createElement('div')
+    row.setAttribute('class', 'row')
+    container.appendChild(row)
+    for (let i = 0; i < width; i++) {
+        const newDiv = document.createElement('div')
+        newDiv.setAttribute('class', 'square')
+        row.appendChild(newDiv)
+    }
+}
+
+
