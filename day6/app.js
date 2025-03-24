3e0a351d23232bd83ced01a80bbcc2f615f174b1
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-03-24 20:23:28 -0300

    Working: Mouse hover correctly changes the pixel color

--- day6/app.js
+++ day6/app.js
@@ -4,14 +4,18 @@ const container = document.querySelector('#table')
 const height = 16
 const width = 16
 
-
+// Creates a row div element to contain the squares in a flex container
 for (let j = 0; j < height; j++) {
     const row = document.createElement('div')
     row.setAttribute('class', 'row')
     container.appendChild(row)
+    // Makes the squares
     for (let i = 0; i < width; i++) {
         const newDiv = document.createElement('div')
         newDiv.setAttribute('class', 'square')
+        newDiv.addEventListener('mouseover', (hover) => {
+            hover.target.style.backgroundColor = '#000'
+        })
         row.appendChild(newDiv)
     }
 }
