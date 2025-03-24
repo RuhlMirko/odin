5079425baeafba4c184176746fef4b9266ce9e1f
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-03-24 20:47:46 -0300

    Working: Added a button to set the width of the display

--- day6/app.js
+++ day6/app.js
@@ -1,23 +1,38 @@
 'use strict'
 
 const container = document.querySelector('#table')
-const height = 16
-const width = 16
+const userBtn = document.querySelector('button')
+let height = 16
+let width = 16
+let pixelFrame = ''
 
-// Creates a row div element to contain the squares in a flex container
-for (let j = 0; j < height; j++) {
-    const row = document.createElement('div')
-    row.setAttribute('class', 'row')
-    container.appendChild(row)
-    // Makes the squares
-    for (let i = 0; i < width; i++) {
-        const newDiv = document.createElement('div')
-        newDiv.setAttribute('class', 'square')
-        newDiv.addEventListener('mouseover', (hover) => {
-            hover.target.style.backgroundColor = '#000'
-        })
-        row.appendChild(newDiv)
+const createSketch = function () {
+    pixelFrame = document.createElement('div')
+    container.appendChild(pixelFrame)
+
+    // Creates a row div element to contain the squares in a flex container
+    for (let j = 0; j < height; j++) {
+        const row = document.createElement('div')
+        row.setAttribute('class', 'row')
+        pixelFrame.appendChild(row)
+        // Makes the squares
+        for (let i = 0; i < width; i++) {
+            const newPixel = document.createElement('div')
+            newPixel.setAttribute('class', 'square')
+            newPixel.addEventListener('mouseover', (hover) => {
+                hover.target.style.backgroundColor = '#000'
+            })
+            row.appendChild(newPixel)
+        }
     }
 }
 
+userBtn.addEventListener('click', function () {
+    width = height = Number(prompt('Enter the desired width'))
+    container.removeChild(pixelFrame)
+    createSketch()
+    console.log(container, width, height);
+})
+
+createSketch()
 
