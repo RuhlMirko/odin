7902051db83c993d20fb47b1a2a27025c40d253d
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-03-25 11:23:13 -0300

    Practice: object exercises part 1 mdn

--- day7/app.js
+++ day7/app.js
@@ -1,1 +1,26 @@
 'use strict'
+/*
+const cat = {
+    name: 'Bertie',
+    breed: 'Cymric',
+    color: 'white',
+    greeting: function () {
+        console.log('Meow!');
+    }
+}
+// above dont edit
+const section = document.querySelector('div')
+let catName = cat.name
+cat.greeting()
+cat.color = 'black'
+
+// below dont edit
+let para1 = document.createElement('p');
+let para2 = document.createElement('p');
+
+para1.textContent = `The cat's name is ${catName}.`;
+para2.textContent = `The cat's color is ${cat.color}.`;
+
+section.appendChild(para1);
+section.appendChild(para2);
+*/
